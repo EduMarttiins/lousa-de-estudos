bc7fd3916b86e467dee8df08c487614513a0b311
EduMarttiins <122230331+EduMarttiins@users.noreply.github.com>
2026-09-24 12:22:32 -0300

    v100: renova cache com barra de desempenho

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const VERSION='99-educacao-fisica-mcq';
+const VERSION='100-score-educacao-fisica';
 const CACHE='lousa-de-estudos-v'+VERSION;
 const ASSETS=[
-  './start.html','./rescue.html','./index.html','./v68.html','./loader-v97.js?v=99',
-  './v37.css?v=99','./pwa-v39.css?v=99','./v37.js?v=99','./v41.js?v=99','./v50.js?v=99','./v54.js?v=99',
-  './v55.js?v=99','./v56.js?v=99','./v57.js?v=99','./v58.js?v=99','./v59.js?v=99','./v60.js?v=99',
-  './v62.js?v=99','./v63.js?v=99','./v64.js?v=99','./v65.js?v=99','./v66.js?v=99','./v67.js?v=99','./v68.js?v=99','./v69.js?v=99',
-  './v70-data-01.js?v=99','./v70-data-02.js?v=99','./v70-data-03.js?v=99','./v70-data-04.js?v=99','./v70-data-05.js?v=99','./v70-data-06.js?v=99','./v70-data-07.js?v=99','./v70-data-08.js?v=99','./v70-data-09.js?v=99','./v70-data-10.js?v=99','./v70-data-11.js?v=99',
-  './v70.js?v=99','./v71.js?v=99','./v72.js?v=99','./v73.js?v=99','./v74.js?v=99','./v75.js?v=99','./v76.js?v=99','./v77.js?v=99','./v78.js?v=99','./v79.js?v=99','./v80.js?v=99','./v81.js?v=99','./v82.js?v=99',
-  './v90-jp.js?v=99','./v90-dn.js?v=99','./v97-math7.js?v=99','./v97-core.js?v=99','./v97-ui.js?v=99','./v99-pe.js?v=99','./manifest.webmanifest?v=99','./icons/lousa-icon-192.png?v=99','./icons/lousa-icon-512.png?v=99'
+  './start.html','./rescue.html','./index.html','./v68.html','./loader-v97.js?v=100',
+  './v37.css?v=100','./pwa-v39.css?v=100','./v37.js?v=100','./v41.js?v=100','./v50.js?v=100','./v54.js?v=100',
+  './v55.js?v=100','./v56.js?v=100','./v57.js?v=100','./v58.js?v=100','./v59.js?v=100','./v60.js?v=100',
+  './v62.js?v=100','./v63.js?v=100','./v64.js?v=100','./v65.js?v=100','./v66.js?v=100','./v67.js?v=100','./v68.js?v=100','./v69.js?v=100',
+  './v70-data-01.js?v=100','./v70-data-02.js?v=100','./v70-data-03.js?v=100','./v70-data-04.js?v=100','./v70-data-05.js?v=100','./v70-data-06.js?v=100','./v70-data-07.js?v=100','./v70-data-08.js?v=100','./v70-data-09.js?v=100','./v70-data-10.js?v=100','./v70-data-11.js?v=100',
+  './v70.js?v=100','./v71.js?v=100','./v72.js?v=100','./v73.js?v=100','./v74.js?v=100','./v75.js?v=100','./v76.js?v=100','./v77.js?v=100','./v78.js?v=100','./v79.js?v=100','./v80.js?v=100','./v81.js?v=100','./v82.js?v=100',
+  './v90-jp.js?v=100','./v90-dn.js?v=100','./v97-math7.js?v=100','./v97-core.js?v=100','./v97-ui.js?v=100','./v99-pe.js?v=100','./v100-score.js?v=100','./manifest.webmanifest?v=100','./icons/lousa-icon-192.png?v=100','./icons/lousa-icon-512.png?v=100'
 ];
 self.addEventListener('install',event=>{event.waitUntil((async()=>{const cache=await caches.open(CACHE);await cache.addAll(ASSETS);})());});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith('lousa-de-estudos-v')&&k!==CACHE).map(k=>caches.delete(k)));})());});
